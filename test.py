f1e6a7f548719853a4a3c2b95c5fed187529ed7d
asya-schtirlitz <i+github@homb.it>
2016-02-15 14:09:24 +0300

    create rss-file

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-#test
+
+from feedgen.feed import FeedGenerator
+fg = FeedGenerator()
+fg.title('test_feed')
+fg.link(href='https://homb.it/podcasts/')
+fg.description('enjoy')
+rssfeed=fg.rss_str(pretty=True)
+fg.rss_file('test.xml')
