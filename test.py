5c66af5d2fc18e4ccea4d7390f583e584cede629
asya-schtirlitz <i+github@homb.it>
2016-02-17 19:42:15 +0300

    stay tunes

--- test.py
+++ test.py
@@ -4,5 +4,14 @@ fg = FeedGenerator()
 fg.title('test_feed')
 fg.link(href='https://homb.it/podcasts/')
 fg.description('enjoy')
+
+fg.load_extension('podcast')
+fg.podcast.itunes_explicit('no')
+fg.podcast.itunes_author('Иван Иванов')
+fg.podcast.itunes_subtitle('Иваново детство')
+fg.podcast.itunes_summary('Детство Ивана')
+fg.podcast.itunes_owner(name='Anastasia M.', email='i@homb.it')
+
 rssfeed=fg.rss_str(pretty=True)
 fg.rss_file('test.xml')
+
